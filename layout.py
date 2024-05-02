bc642f8e99cc51fe42ed8a27f175c0182023b5dc
Le Hong Anh <138895020+anhlehong@users.noreply.github.com>
2024-05-02 22:15:56 +0700

    Update layout.py

--- layout.py
+++ layout.py
@@ -4,45 +4,43 @@ import streamlit as st
 def layout():
     st.title("Depress Detection")
 
-    input_col, analysis_col = st.columns((1.5, 1))
+    input_col, analysis_col = st.columns((2, 1))
 
     with input_col:
         st.header("Input")
 
-        if 'user_input' not in st.session_state:
+        if st.session_state.get("reset"):
             st.session_state.user_input = ""
-        user_input = st.text_area("", height=300, key="input", value=st.session_state.user_input)
 
-        col1, col2, col3 = st.columns((0.3, 0.5, 0.2))
+        st.text_area("", height=350, key="user_input")
+
+        col1, col2, col3 = st.columns((0.2, 0.55, 0.15))
         with col1:
-            detect_pressed = st.button("Detect")
+            st.button("Detect", key="detect")
         with col2:
             st.write("")
         with col3:
-            reset_pressed = st.button("Reset")
-            if reset_pressed:
-                st.session_state.user_input = ""
-                st.experimental_rerun()
+            st.button("Reset", key="reset")
 
     output_col = st.container()
 
-    with analysis_col:
-        st.header("Analysis")
+    if st.session_state.get("detect") and st.session_state.get("user_input"):
+        with analysis_col:
+            st.header("Analysis")
+            st.write("")
 
-        if detect_pressed:
             st.subheader("Ba từ tiêu cực nhất")
-            st.write(user_input)
+            st.write(st.session_state.get("user_input"))
 
             st.subheader("Câu tiêu cực nhất")
-            st.write(user_input)
+            st.write(st.session_state.get("user_input"))
 
             st.subheader("Topic")
-            st.write(user_input)
+            st.write(st.session_state.get("user_input"))
 
-    with output_col:
-        if detect_pressed:
+        with output_col:
             st.subheader("Output")
-            st.markdown(user_input)
+            st.write(st.session_state.get("user_input"))
 
 
 if __name__ == "__main__":
